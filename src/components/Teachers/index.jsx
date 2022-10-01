e6977f6251d568cc6e7ac489fa18c4efda65b8b1
NAbdullokh <country.abu@gmail.com>
2022-10-02 02:16:59 +0500

    test

--- src/components/Teachers/index.jsx
+++ src/components/Teachers/index.jsx
@@ -1,8 +1,49 @@
 import React from "react";
-import { Container, Wrapper } from "./style";
+import { Card, CardWrapper, Container, Wrapper } from "./style";
 import document from "../../assets/icon/document.svg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { teacherData } from "../../mock/teacherData";
+import Button from "../../generic/Button";
 
 const Teachers = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Wrapper>
       <Container>
@@ -13,6 +54,20 @@ const Teachers = () => {
         </div>
         <p className="title">BIZNING USTOZLARIMIZ</p>
         <p className="desc">BIZNING FAXRIMIZ</p>
+        <CardWrapper>
+          <Slider {...settings}>
+            {teacherData.map((value) => {
+              return (
+                <Card>
+                  <img src={value.img} alt={value.name} />
+                  <Card.Name>{value.name}</Card.Name>
+                  <Card.Course>{value.course}</Card.Course>
+                  <Button>Batafsil</Button>
+                </Card>
+              );
+            })}
+          </Slider>
+        </CardWrapper>
       </Container>
     </Wrapper>
   );
--- src/components/Teachers/style.js
+++ src/components/Teachers/style.js
@@ -12,9 +12,44 @@ export const Container = styled.div`
   max-width: var(--maxWidth);
   margin: auto;
   padding: 25px 130px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  /* display: flex;
+  flex-direction: column; */
+  /* align-items: center; */
 `;
 
 Container.Img = styled.img``;
+
+export const CardWrapper = styled.div`
+  margin-top: 25px;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 25px 30px;
+  background-color: white;
+`;
+
+Card.Name = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  /* display: flex; */
+  /* align-items: center; */
+  /* text-align: center; */
+  margin: auto;
+  color: #000000;
+`;
+
+Card.Course = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 37px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #009f3c;
+`;
