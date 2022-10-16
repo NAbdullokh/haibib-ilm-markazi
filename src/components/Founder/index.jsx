35b2196e8428eee63cfdd219fbbe71623cdaaf03
NAbdullokh <country.abu@gmail.com>
2022-10-16 21:28:52 +0500

    test

--- src/components/Founder/index.jsx
+++ src/components/Founder/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Box1, Box2, BoxWrapper, Container, Wrapper } from "./style";
+import {
+  Box1,
+  Box2,
+  BoxWrapper,
+  BtnWrapper,
+  Container,
+  Wrapper,
+} from "./style";
 import userImg from "../../assets/icon/user.svg";
+import Button from "../../generic/Button";
+import founderImg from "../../assets/img/founder.png";
 
 const Founder = () => {
   return (
@@ -17,11 +26,24 @@ const Founder = () => {
           <Box1>
             <div className="title">ASSALOMU ALEYKUM! </div>
             <Box1.Head>
-              Agarda qandaydir shikoyat yoki taklifingiz bo’lsa,{" "}
+              Agarda qandaydir shikoyat yoki taklifingiz bo’lsa,
               <span>shaxsan o’zim eshitishga tayyorman</span>
             </Box1.Head>
+            <div className="titleWrap" style={{ width: "100%" }}>
+              <Box1.Input type="text" placeholder="Ismingiz" />
+              <Box1.Input type="number" defaultValue="+998" />
+            </div>
+            <Box1.TextArea placeholder="Shikoyat va takliflaringizni yozing" />
+
+            <BtnWrapper>
+              <Button>YUBORISH</Button>
+            </BtnWrapper>
           </Box1>
-          <Box2></Box2>
+          <Box2>
+            <Box2.Img src={founderImg} />
+            <p className="desc">HABIBULLOH AHMADOV</p>
+            <Box1.Head>“HABIB - ILM MARKAZI” ASOSCHISI</Box1.Head>
+          </Box2>
         </BoxWrapper>
       </Container>
     </Wrapper>
--- src/components/Founder/style.js
+++ src/components/Founder/style.js
@@ -15,6 +15,12 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media (max-width: 1450px) {
+    padding: 25px 50px;
+  }
+  @media (max-width: 400px) {
+    padding: 25px 20px;
+  }
 `;
 
 Container.Img = styled.img``;
@@ -22,9 +28,17 @@ Container.Img = styled.img``;
 export const BoxWrapper = styled.div`
   display: flex;
   width: 100%;
+  gap: 50px;
+  margin: 25px 0;
+  @media (max-width: 900px) {
+    flex-direction: column-reverse;
+  }
 `;
 export const Box1 = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 
 Box1.Head = styled.p`
@@ -32,12 +46,63 @@ Box1.Head = styled.p`
   font-weight: 400;
   font-size: 24px;
   line-height: 28px;
+  text-align: center;
   color: #ffffff;
   span {
     font-weight: 700;
   }
 `;
 
+Box1.Input = styled.input`
+  width: 100%;
+  height: 50px;
+  background: #d9d9d9;
+  border-radius: 10px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+Box1.TextArea = styled.textarea`
+  width: 100%;
+  height: 200px;
+  padding-left: 10px;
+  border-radius: 10px;
+  background: #d9d9d9;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  color: rgba(0, 0, 0, 0.6);
+  margin: 25px 0;
+`;
+
+export const BtnWrapper = styled.div`
+  width: 400px;
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+`;
+
 export const Box2 = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  border-radius: 30px;
+  justify-content: center;
+  gap: 10px;
+`;
+Box2.Img = styled.img`
+  width: 100%;
+  height: 100%;
 `;
